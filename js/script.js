2d0b32f8fbf25e60224e6b16bbd3c063f73c7914
Zakki101 <muhammadzakkiarifin@gmail.com>
2025-12-28 06:28:50 +0700

    navbar

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-// // buat hover effect logo
-// const logo = document.getElementById("logo");
-// const text = document.getElementById("logoText");
+// buat hover effect logo
+const logo = document.getElementById("logo");
+const text = document.getElementById("logoText");
 
-// let holdTimer;
+let holdTimer;
 
-// /* Klik & tahan (mouse / touch) */
-// logo.addEventListener("mousedown", showText);
-// logo.addEventListener("mouseup", hideText);
-// logo.addEventListener("mouseleave", hideText);
+/* Klik & tahan (mouse / touch) */
+logo.addEventListener("mousedown", showText);
+logo.addEventListener("mouseup", hideText);
+logo.addEventListener("mouseleave", hideText);
 
-// /* Touch (Mobile) */
-// logo.addEventListener("touchstart", showText);
-// logo.addEventListener("touchend", hideText);
-// logo.addEventListener("touchcancel", hideText);
-// function showText() {
-//     text.style.opacity = "1";
-//     text.style.visibility = "visible";
-// }
+/* Touch (Mobile) */
+logo.addEventListener("touchstart", showText);
+logo.addEventListener("touchend", hideText);
+logo.addEventListener("touchcancel", hideText);
+function showText() {
+    text.style.opacity = "1";
+    text.style.visibility = "visible";
+}
 
-// function hideText() {
-//     text.style.opacity = "0";
-//     text.style.visibility = "hidden";
-// }
+function hideText() {
+    text.style.opacity = "0";
+    text.style.visibility = "hidden";
+}
